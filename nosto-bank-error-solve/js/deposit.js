16c9ab3f2e36ca55ddd7e31faa272909d141362b
mirza-azam <mirzaazam884@gmail.com>
2023-12-11 12:15:26 +0600

    utilites error fixed

--- nosto-bank-error-solve/js/deposit.js
+++ nosto-bank-error-solve/js/deposit.js
@@ -6,22 +6,15 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     3. convert string value to a number
     */
   const newDepositAmount = getInputFieldValueById("deposit-field");
+  setTextElementValueById("deposit-total", newDepositAmount);
   /* 
     1. get previous deposit total by id
 
     */
   const previousDepositTotal = getTextElementValueById("deposit-total");
-  // const balanceTotal = getTextElementValueById("balance-total");
-
-  // calculate new deposit total
-  const balanceTotalCalculate = previousDepositTotal + balanceTotal;
-  // console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
-  // set deposit total value
-  setTextElementValueById("deposit-total", newDepositAmount);
-  // setTextElementValueById("balance-total", balanceTotalCalculate);
 
   // get previous balance by using the function
-  const previousBalanceTotal = document.getElementById("balance-total");
-  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  const previousBalanceTotal = getTextElementValueById('balance-total')
+  const newBalanceTotal = previousBalanceTotal + previousDepositTotal;
   setTextElementValueById("balance-total", newBalanceTotal);
 });
--- nosto-bank-error-solve/js/utilities.js
+++ nosto-bank-error-solve/js/utilities.js
@@ -2,15 +2,15 @@ function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
-  return inputFieldValue;
   inputField.value = "";
+  return inputFieldValue;
 }
 
 function getTextElementValueById(elementId) {
   const textElement = document.getElementById(elementId);
   const textElementValueString = textElement.innerText;
-  const textElementValue = textElementValueString;
-  textElementValue;
+  const textElementValue = parseFloat(textElementValueString);
+  return textElementValue;
 }
 
 function setTextElementValueById(elementId, newValue) {
