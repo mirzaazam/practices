f8989b89d94b4844c9008792cca2afe963fd42f0
mirza-azam <mirzaazam884@gmail.com>
2023-12-13 22:10:46 +0600

    get & set

--- localStorsge-save&get/js/script.js
+++ localStorsge-save&get/js/script.js
@@ -9,9 +9,34 @@ const getItems = () =>{
     console.log(productValue,quantityValue);
 }
 
+//push the items in ul container 
 const pushItemsOnListContainer = (product, quantity) =>{
     const listContainer = document.getElementById('list-container');
     const li = document.createElement('li');
     li.innerText = `${product}: ${quantity}`;
     listContainer.appendChild(li);
+    // getStoreValueFromLocalstorage();
+    saveItemToLocalstorage(product,quantity);
 }
+
+
+
+//Get items on local storage in the browser
+const getStoreValueFromLocalstorage = () =>{
+    let cart = {};
+    const stored = localStorage.getItem('cart');
+    if(stored){
+        cart = JSON.stringify(stored);
+    }
+    return cart;
+}
+
+// Get items on local storage in the browser
+const saveItemToLocalstorage = (product, quantity) =>{
+   const cart = getStoreValueFromLocalstorage();
+   cart[product] = quantity;
+   const stringifyToobject = JSON.stringify(cart);
+   localStorage.setItem('cart', stringifyToobject);
+}
+
+// getStoreValueFromLocalstorage();
