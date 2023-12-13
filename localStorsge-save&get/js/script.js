40d0c0ee887f97c9e666fa1d82084e0e4c731410
mirza-azam <mirzaazam884@gmail.com>
2023-12-13 12:53:24 +0600

    get product and quantity value from input field worked now

---
+++ localStorsge-save&get/js/script.js
@@ -0,0 +1,8 @@
+const getItems = () =>{
+    const product = document.getElementById('product');
+    const quantity = document.getElementById('quantity');
+    const productValue = product.value;
+    const quantityValue = quantity.value;
+
+    console.log(productValue,quantityValue);
+}
