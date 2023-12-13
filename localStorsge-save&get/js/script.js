5c112618837ec455442df6f2e7177ec5eccb6fa2
mirza-azam <mirzaazam884@gmail.com>
2023-12-13 14:30:56 +0600

    create ul container than push tthe list child

--- localStorsge-save&get/js/script.js
+++ localStorsge-save&get/js/script.js
@@ -3,6 +3,15 @@ const getItems = () =>{
     const quantity = document.getElementById('quantity');
     const productValue = product.value;
     const quantityValue = quantity.value;
-
+    product.value = '';
+    quantity.value = '';
+    pushItemsOnListContainer(productValue,quantityValue);
     console.log(productValue,quantityValue);
+}
+
+const pushItemsOnListContainer = (product, quantity) =>{
+    const listContainer = document.getElementById('list-container');
+    const li = document.createElement('li');
+    li.innerText = `${product}: ${quantity}`;
+    listContainer.appendChild(li);
 }
