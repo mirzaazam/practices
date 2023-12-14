933e14cdf925b76ec54ded3bc50c4bbb79dadb44
mirza-azam <mirzaazam884@gmail.com>
2023-12-14 07:22:27 +0600

    fix that

--- localStorsge-save&get/js/script.js
+++ localStorsge-save&get/js/script.js
@@ -1,42 +1,34 @@
-const getItems = () =>{
+const getItems = () => {
     const product = document.getElementById('product');
     const quantity = document.getElementById('quantity');
     const productValue = product.value;
     const quantityValue = quantity.value;
     product.value = '';
     quantity.value = '';
-    pushItemsOnListContainer(productValue,quantityValue);
-    console.log(productValue,quantityValue);
+    pushItemsOnListContainer(productValue, quantityValue);
+    console.log(productValue, quantityValue);
 }
 
-//push the items in ul container 
-const pushItemsOnListContainer = (product, quantity) =>{
+const pushItemsOnListContainer = (product, quantity) => {
     const listContainer = document.getElementById('list-container');
     const li = document.createElement('li');
     li.innerText = `${product}: ${quantity}`;
     listContainer.appendChild(li);
-    // getStoreValueFromLocalstorage();
-    saveItemToLocalstorage(product,quantity);
+    saveItemToLocalstorage(product, quantity);
 }
 
-
-
-//Get items on local storage in the browser
-const getStoreValueFromLocalstorage = () =>{
+const getStoreValueFromLocalstorage = () => {
     let cart = {};
     const stored = localStorage.getItem('cart');
-    if(stored){
-        cart = JSON.stringify(stored);
+    if (stored) {
+        cart = JSON.parse(stored); // Change here
     }
     return cart;
 }
 
-// Get items on local storage in the browser
-const saveItemToLocalstorage = (product, quantity) =>{
-   const cart = getStoreValueFromLocalstorage();
-   cart[product] = quantity;
-   const stringifyToobject = JSON.stringify(cart);
-   localStorage.setItem('cart', stringifyToobject);
+const saveItemToLocalstorage = (product, quantity) => {
+    const cart = getStoreValueFromLocalstorage();
+    cart[product] = quantity;
+    const stringifyToobject = JSON.stringify(cart);
+    localStorage.setItem('cart', stringifyToobject);
 }
-
-// getStoreValueFromLocalstorage();
