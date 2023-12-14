01f10d212820888a766840dcc1bb35113462a6b4
mirza-azam <mirzaazam884@gmail.com>
2023-12-14 21:52:19 +0600

    added set & remove from localstorage process

---
+++ localStorsge-save&get/local-storeage-lavel-3/script.js
@@ -0,0 +1,44 @@
+
+// for delete 
+const forDeleteBtn = (stringValue) =>{
+    localStorage.removeItem(stringValue);
+}   
+
+//for send btn
+const forSendBtn = (str,value) =>{
+    localStorage.setItem(str, value);
+}
+
+// common for get input value and converted string for set in local storeage
+const forSendButton = (input, key) => {
+    const inputValue = document.getElementById(input).value;  
+    if(!inputValue){
+        alert('Put the value!');
+    }
+    else{
+        const stringValue = JSON.stringify(inputValue);
+        document.getElementById(input).value = ''
+        forSendBtn(key, stringValue);
+    }
+}
+
+//For name delete btn
+document.getElementById('delete-name-btn').addEventListener('click', function(){
+    forDeleteBtn('name');
+});
+
+//For name send btn
+document.getElementById('send-name-btn').addEventListener('click', function(){
+    forSendButton('name-input', 'name');
+});
+
+// for delete email
+document.getElementById('delete-email-name').addEventListener('click', function(){
+    forDeleteBtn('email');
+}); 
+
+// for send email
+document.getElementById('send-email-btn').addEventListener('click', function(){
+    forSendButton('email-field', 'email');
+}); 
+
