b2ff8f38b4a080900f3519010cf61f60cf7a72ea
mirza-azam <mirzaazam884@gmail.com>
2023-12-17 00:12:01 +0600

    repeated content error fix

--- instra-error-problem/js/app.js
+++ instra-error-problem/js/app.js
@@ -144,8 +144,8 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  document.getElementById("liked").textContent = '';
   likedPosts.forEach((post) => {
-    console.log(post)
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
   });
@@ -153,6 +153,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
+  document.getElementById("reported").textContent = '';
   reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
